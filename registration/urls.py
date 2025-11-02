8fc7d8efdd42498428bd3f2af80bff93ed944d51
sphil5 <195391783+sphil5@users.noreply.github.com>
2025-11-01 23:26:27 -0500

    updated registration
    I updated a few things within registration and created html files. However, right now those files are empty.

--- registration/urls.py
+++ registration/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include 
-from users import views as user_views
+from registration import views as registration_views
 from django.contrib.auth import views as auth_views
 from django.shortcuts import redirect
 
@@ -9,5 +9,10 @@ def redirect_to_login(request):
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("register/", user_views.register, name = "register")
+    path("register/", registration_views.register, name = "register")
+    
+    # I think we are going to need these paths
+    # path("profile/", registration_views.profile, name = "profile"),
+    # path("login/", auth_views.LoginView.as_view(template_name = "registration/login.html"), name = "login")
+    # path("logout/", auth_views.LogoutView.as_view(template_name = "registration/logout.html", next_page = "login"), name = "logout")
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,23 +1,31 @@
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages 
+from django.contrib.auth.decorators import login_required 
+#from django.views.decorators.csrf import csrf_exempt
+#from django.http import JsonResponse
 from .forms import UserRegisterForm
-import json
+#import json
 
 # Create your views here.
 
+
 def register(request):
     if request.method == "POST":
-        data = json.loads(request.body)
-        form = UserRegisterForm(data)
+        #data = json.loads(request.body)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            return JsonResponse({"success": True, "message": "Account created"}, status=201)
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Account was successfully created for: {username}.")
+            return redirect('login')
         else:
-            return JsonResponse({"success": False, "errors": form.errors}, status=400)
-        
-    return JsonResponse({"detail": "Method not allowed"}, status=405)
+            form = UserRegisterForm()
+        return render(request, "registration/register.html", {"form":form}) # taking users to their profile
     
+@login_required
+def profile(request):
+    return render(request, "registration/profile.html")
+            
 
             
 
