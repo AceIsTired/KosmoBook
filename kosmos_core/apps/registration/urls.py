e82c24ba2ed2b539caf3dd799abc0e2daeccc050
sphil5 <195391783+sphil5@users.noreply.github.com>
2025-11-03 23:10:20 -0600

    Update urls.py

--- kosmos_core/apps/registration/urls.py
+++ kosmos_core/apps/registration/urls.py
@@ -1,20 +1,9 @@
-from django.contrib import admin
 from django.urls import path, include 
-from registration import views as registration_views
-from django.contrib.auth import views as auth_views
-from django.shortcuts import redirect
 from . import views
 
-def redirect_to_login(request):
-    return redirect('login')
+app_name = 'registration'
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("register/", views.register, name = "register"),
     path("profile/", views.profile, name = "profile"),
-    # I think we are going to need these paths
-    # path("profile/", registration_views.profile, name = "profile"),
-    # path("login/", auth_views.LoginView.as_view(template_name = "registration/login.html"), name = "login")
-    # path("logout/", auth_views.LogoutView.as_view(template_name = "registration/logout.html", next_page = "login"), name = "logout")
 ]
-
-app_name = 'registration'
