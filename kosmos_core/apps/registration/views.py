1866cc3ffd06bfd7b25f89760bb8512323a17281
sphil5 <195391783+sphil5@users.noreply.github.com>
2025-11-13 19:58:49 -0600

    Update views.py

--- kosmos_core/apps/registration/views.py
+++ kosmos_core/apps/registration/views.py
@@ -7,15 +7,12 @@ from .forms import UserRegisterForm
 
 
 def register(request):
-    if request.method == "POST":
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f"Account was successfully created for: {username}.")
-            return redirect('login')
-    else:
-        form = UserRegisterForm()
+    form = UserRegisterForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        user = form.save()
+        messages.success(request, f"Account was successfully created for: {user.username}.")
+        return redirect('login')
 
     return render(request, "registration/register.html", {"form":form})
 
