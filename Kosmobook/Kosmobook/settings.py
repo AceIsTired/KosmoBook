23300f86f9e3d72f3838aa7c256a732796132c28
AceIsTired <162749835+AceIsTired@users.noreply.github.com>
2025-12-02 11:32:07 -0600

    Update settings.py

--- Kosmobook/Kosmobook/settings.py
+++ Kosmobook/Kosmobook/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'authentication',
+    'appointments',
 ]
 
 MIDDLEWARE = [
@@ -124,5 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 AUTH_USER_MODEL = 'users.CustomUser'
