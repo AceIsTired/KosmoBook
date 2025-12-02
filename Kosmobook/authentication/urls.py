ff2bc15a5b453474442031aa33809c52882cd073
AceIsTired <162749835+AceIsTired@users.noreply.github.com>
2025-12-02 11:29:07 -0600

    Delete Kosmobook directory

--- Kosmobook/authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.landing, name='landing'),
-    path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
-]
