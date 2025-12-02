069d29afdaa04ee8bed1764a90fd9213ede94f5d
AceIsTired <162749835+AceIsTired@users.noreply.github.com>
2025-12-02 09:41:44 -0600

    Update tests.py

--- Kosmobook/users/tests.py
+++ Kosmobook/users/tests.py
@@ -29,28 +29,28 @@ class ProfileViewTests(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
         
-# lets integrate this test later on
-# def test_setup_forms(self):
 
-#     user_type_form = UserTypeForm(data={'user_type': 'user'})
-#     self.assertTrue(user_type_form.is_valid())
-#     self.assertEqual(user_type_form.cleaned_data['user_type'], 'user')
+    def test_setup_forms(self):
 
-#     profile_form = ProfileInfoForm(data={
-#         'location': 'Los Angeles, CA',
-#         'bio': 'I am a beauty enthusiast'
-#     })
-#     self.assertTrue(profile_form.is_valid())
-#     self.assertEqual(profile_form.cleaned_data['location'], 'Los Angeles, CA')
+    user_type_form = UserTypeForm(data={'user_type': 'user'})
+    self.assertTrue(user_type_form.is_valid())
+    self.assertEqual(user_type_form.cleaned_data['user_type'], 'user')
 
-#     tech_form = TechnicianDetailsForm(data={
-#         'specialty': 'makeup',
-#         'years_of_experience': 3,
-#         'licensed': True
-#     })
-#     self.assertTrue(tech_form.is_valid())
-#     self.assertEqual(tech_form.cleaned_data['specialty'], 'makeup')
-#     self.assertTrue(tech_form.cleaned_data['licensed'])
+    profile_form = ProfileInfoForm(data={
+        'location': 'Los Angeles, CA',
+        'bio': 'I am a beauty enthusiast'
+    })
+    self.assertTrue(profile_form.is_valid())
+    self.assertEqual(profile_form.cleaned_data['location'], 'Los Angeles, CA')
+
+    tech_form = TechnicianDetailsForm(data={
+        'specialty': 'makeup',
+        'years_of_experience': 3,
+        'licensed': True
+    })
+    self.assertTrue(tech_form.is_valid())
+    self.assertEqual(tech_form.cleaned_data['specialty'], 'makeup')
+    self.assertTrue(tech_form.cleaned_data['licensed'])
         
 
 # Some profile tests that we can run once the profile is connected to the home page and login down below
